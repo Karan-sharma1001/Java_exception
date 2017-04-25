1ec784c72108b6aed27d53bce265fd2dfa8a26b4
Karan-sharma1001 <sharma.karan.b94@gmail.com>
2017-04-25 16:10:53 +0530

    Add files via upload

---
+++ assign_9/src/assign_9/FileDemo.java
@@ -0,0 +1,23 @@
+package assign_9;
+
+import java.io.*;
+
+public class FileDemo {
+
+	public static void main(String[] args) {
+		FileReader fis;
+		try {
+		 fis=new FileReader("input.dat");
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
